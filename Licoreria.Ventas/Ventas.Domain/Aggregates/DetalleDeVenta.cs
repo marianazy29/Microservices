ab0e00d1f7858f5a23ea54dcf037998df7f27e58
Mariana Zúñiga <mariana.zuniga@ucb.edu.bo>
2025-11-05 00:06:25 -0400

    Se completo la Capa Domain de Ventas y Cliente

---
+++ Licoreria.Ventas/Ventas.Domain/Aggregates/DetalleDeVenta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventas.Domain.Aggregates
+{
+    public class DetalleDeVenta
+    {
+        public Guid Id { get; private set; }
+        public Guid VentaId { get; private set; }
+        public Guid ProductoId { get; private set; }
+        public int Cantidad { get; private set; }
+        public double Precio { get; private set; }
+        public double Descuento { get; private set; }
+        public double TotalLinea { get; private set; }
+        public string Estado { get; private set; }
+
+        public DetalleDeVenta(Guid ventaId, Guid productoId, int cantidad, double precio, double descuento)
+        {
+            Id = Guid.NewGuid();
+            VentaId = ventaId;
+            ProductoId = productoId;
+            Cantidad = cantidad;
+            Precio = precio;
+            Descuento = descuento;
+            TotalLinea = (cantidad * precio) -  descuento;
+            Estado = "ACTIVO";
+        }
+
+        public void CambiarEstado(string estado)
+        {
+            Estado = estado;
+        }
+
+        public void ActualizarCantidad(int nuevaCantidad)
+        {
+            if (nuevaCantidad < 1)
+                throw new ArgumentException("Cantidad debe ser mayor que cero.");
+            Cantidad = nuevaCantidad;
+        }
+        public void ActualizarTotalLinea(double cantidad, double precio, double descuento)
+        {
+            TotalLinea = (cantidad * precio) - descuento; ;
+        }
+    }
+}
+
--- Licoreria.Ventas/Ventas.Domain/Aggregates/Venta.cs
+++ Licoreria.Ventas/Ventas.Domain/Aggregates/Venta.cs
@@ -9,22 +9,58 @@ namespace Ventas.Domain.Aggregates
     public class Venta
     {
         public Guid Id { get; private set; }
+        public Guid UsuarioId { get; private set; }
         public Guid ClienteId { get; private set; }
-        public decimal Descuento { get; private set; }
-        public decimal Monto { get; private set; }
+        public double Descuento { get; private set; }
+        public double MontoTotal { get; private set; }
         public DateTime Fecha { get; private set; }
         public string Comentarios { get; private set; }
         public string Estado { get; private set; }
 
-        public Venta(Guid id, Guid clienteId, decimal monto,string comentarios)
+        private readonly List<DetalleDeVenta> _detalles = new();
+        public IReadOnlyCollection<DetalleDeVenta> Detalles => _detalles.AsReadOnly();
+        public Venta(Guid id, Guid clienteId, double montoTotal,string comentarios)
         {
             Id = id;
             ClienteId = clienteId;
-            Monto = monto;
+            MontoTotal = montoTotal;
             Fecha = DateTime.UtcNow;
             Comentarios = comentarios;
             Estado = "ACTIVA";
         }
 
+        public void AgregarDetalle(Guid productoId, int cantidad, double precio, double descuento)
+        {
+            if (cantidad < 1)
+                throw new ArgumentException("La cantidad debe ser mayor que cero.");
+
+            if (precio <= 0)
+                throw new ArgumentException("El precio debe ser mayor que cero.");
+
+            var detalle = new DetalleDeVenta(Id, productoId, cantidad, precio, descuento);
+            _detalles.Add(detalle);
+            ActualizarMontoTotal();
+        }
+
+        public void CambiarEstadoDetalle(Guid detalleId, string estado)
+        {
+            var detalle = _detalles.FirstOrDefault(d => d.Id == detalleId);
+            if (detalle == null)
+                throw new InvalidOperationException("Detalle no encontrado.");
+
+            detalle.CambiarEstado(estado);
+            ActualizarMontoTotal();
+        }
+
+        private void ActualizarMontoTotal()
+        {
+            double descuentoTotal = ActualizarDescuento();
+            MontoTotal = _detalles.Where(d => d.Estado == "ACTIVO").Sum(d => d.Precio * d.Cantidad) - descuentoTotal;
+        }
+
+        private double ActualizarDescuento()
+        {
+           return  Descuento = _detalles.Where(d => d.Estado == "ACTIVO").Sum(d => d.Descuento);
+        }
     }
 }
